28c878c7635eabdbf65cd3602bb94af7e40f52b0
Jordan Wright <jordanwright@Jordans-MacBook-Pro.local>
2022-09-07 14:11:34 -0400

    Reformatted Articles index
    In addition, added a description to my articles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_152852) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_07_180000) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
 end
