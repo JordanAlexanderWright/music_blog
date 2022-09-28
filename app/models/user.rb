5a9dda0087bee8b32af0da68f2dfdf633c80a9c2
Jordan Wright <jordanwright@Jordans-MacBook-Pro.local>
2022-09-28 12:57:42 -0400

    Added password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :user_name, uniqueness: { case_sensitive: false }, presence: true, length: {minimum: 3, maximum: 25}
   validates :email, uniqueness: true, presence: true, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_secure_password
 end
