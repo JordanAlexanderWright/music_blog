2cea8d175e4d212cd6c982410b867f9d3c834c0a
Jordan Wright <jordanwright@Jordans-MacBook-Pro.local>
2022-09-28 11:33:50 -0400

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :articles
+  validates :user_name, uniqueness: { case_sensitive: false }, presence: true, length: {minimum: 3, maximum: 25}
+  validates :email, uniqueness: true, presence: true, length: {maximum: 105}, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
